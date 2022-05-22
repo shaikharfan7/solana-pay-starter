82107c879ad9b0f09e5d0e6b14b1d9e64c49c5da
shaikharfan7 <shaikharfan7@gmail.com>
2022-05-22 16:58:38 +0530

    Final

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadComponent from '../components/Head';
-
+import { useWallet } from "@solana/wallet-adapter-react";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import Product from "../components/Product";
+import CreateProduct from "../components/CreateProduct";
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const USERTWITTER_HANDLE = "shaikharfan7"
+const USERTWITTER_LINK = `https://twitter.com/${USERTWITTER_HANDLE}`;
 
 const App = () => {
   
-  
+  const {publicKey} = useWallet();
+  const isOwner = ( publicKey ? publicKey.toString() === process.env.NEXT_PUBLIC_OWNER_PUBLIC_KEY : false );
+  const [creating, setCreating] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  },[publicKey])
+
+  const renderNotConnectedContainer = () =>(
+    <div>
+      <img src="https://media.giphy.com/media/Zgo2A2oOpbGhQdf09T/giphy.gif" alt="emoji" />
+    
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+
+    </div>
+  )
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+
+
   return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
-          <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="header"> 🏢 The Office GIF Store 🏢</p>
+          <p className="sub-text">The only gif store that accepts sh*tcoins</p>
+       
+          {isOwner && (
+            <button className="create-product-button" onClick={() => setCreating(!creating)}>
+              {creating ? "Close" : "Create Product"}
+            </button>
+          )}
+       
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {creating && <CreateProduct />}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+        
+          <a
+            className="footer-text"
+            href={USERTWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`Built By @${USERTWITTER_HANDLE} at => `}</a>
+          
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`@${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
